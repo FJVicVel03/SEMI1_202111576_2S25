aa6132db6272d0f2a8b309c3ef271ff428ebfe21
FJVicVel03 <3021894750101@ingenieria.usac.edu.gt>
2025-09-15 20:12:33 -0600

    feat: api1 y api2 azure

---
+++ HT2/api2.py
@@ -0,0 +1,17 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/get-data', methods=['GET'])
+def get_data():
+    data = {
+        "api_name": "API 2",  # Este valor lo puedes personalizar
+        "data": {
+            "id": 1,
+            "nombre": "Grupo 5 - Semi 1 - Api 2"
+        }
+    }
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
